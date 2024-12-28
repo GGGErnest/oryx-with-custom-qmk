9119e86513c80936ca9e4286f5db9b055ee9892a
Ernesto Pedrosa Alonso <6105449+GGGErnest@users.noreply.github.com>
2024-12-28 10:27:46 +0100

    bypass opposite_hands control on layer switches keys

--- DRG6q/features/achordion.c
+++ DRG6q/features/achordion.c
@@ -335,6 +335,10 @@ __attribute__((weak)) bool achordion_chord(uint16_t tap_hold_keycode,
                                            keyrecord_t* tap_hold_record,
                                            uint16_t other_keycode,
                                            keyrecord_t* other_record) {
+  if(tap_hold_keycode == MO_1 || tap_hold_keycode == MO_2 || tap_hold_keycode == MO_3) {
+    return true;
+  }
+
   return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
