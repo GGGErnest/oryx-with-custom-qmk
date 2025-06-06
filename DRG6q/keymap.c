7d28614aa9c2e25af88173505bc6f7071587fd5e
Ernesto Pedrosa Alonso <6105449+GGGErnest@users.noreply.github.com>
2025-06-06 23:26:09 +0200

    Update keymap.c

--- DRG6q/keymap.c
+++ DRG6q/keymap.c
@@ -225,3 +225,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
+// Exclude specific keys from the opposite hand rule
+bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
+                      uint16_t other_keycode, keyrecord_t* other_record) {
+    // Exceptionally allow some one-handed chords for hotkeys.
+    switch (tap_hold_keycode) {
+        case LT(2,KC_BSPC):
+            return true;
+            break;
+    }
+    // Otherwise defer to the opposite hands rule.
+    return get_chordal_hold_default(tap_hold_record, other_record);
+}
+
+// Custom QMK here
+const key_override_t delete_key_override = 
+    ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&delete_key_override,
+	NULL
+};
